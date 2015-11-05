124620fa790a693812336089290eb2f3f310332f
Benjamin McLean <mclean.ben@gmail.com>
2015-11-05 12:57:12 -0600

    added block pushing mechanic

--- core/src/net/benmclean/badchallenge/controller/GameWorldController.java
+++ core/src/net/benmclean/badchallenge/controller/GameWorldController.java
@@ -44,10 +44,19 @@ public class GameWorldController {
         return world.getSeed();
     }
 
-    public boolean move(Direction direction) {
-        if (eval(posX + direction.dx(), posY + direction.dy()).canStep()) {
-            posX += direction.dx();
-            posY += direction.dy();
+    public boolean move(Direction way) {
+        // If you can push the block in front of you, and it can move into the tile you'd be pushing it onto:
+        if (eval(posX + way.dx(), posY + way.dy()).canPush()
+                && eval(posX + way.dx()*2, posY + way.dy()*2).canPushTo()) {
+            // Set (the tile the block in front of you is moving onto) to be the block:
+            set(posX + way.dx()*2, posY + way.dy()*2, eval(posX + way.dx(), posY + way.dy()));
+            // Set (the tile in front of you) to be (whatever type of tile the block leaves behind after getting pushed):
+            set(posX + way.dx(), posY + way.dy(), eval(posX + way.dx()*2, posY + way.dy()*2).afterPush());
+        }
+        // If you can step onto the tile in front of you:
+        if (eval(posX + way.dx(), posY + way.dy()).canStep()) {
+            posX += way.dx();
+            posY += way.dy();
             return true;
         }
         return false;
